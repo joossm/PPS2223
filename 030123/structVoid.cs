28a5bedf36f83367f556f5c88415161b65c4d8b3
Matthias Jooß <Jooss.Matthias@gmail.com>
2023-01-03 12:25:37 +0100

    4

--- 030123/structVoid.cs
+++ 030123/structVoid.cs
@@ -14,10 +14,17 @@ public  static void u (ref C x){
 class Program
 {
     static void Main(string[] args) {
-		C y = new C {foo=3};
-		C z = y;
+		C y = new C {foo=3};                    // y Objekt von Struct C mit foo = 3
+		C z = y;                                // z Objekt von Struct C mit foo = 3
 		M.u(ref y);
-		Console.WriteLine("y.foo: " + y.foo);
-		Console.WriteLine("z.foo: " + z.foo);
+		// Aufruf der Methode u der Klasse M, mit Übergabe der Speicheradresse des Objektes y
+		/*
+		public  static void u (ref C y){
+        	y.foo=4;                            // foo der Speicheradresse wird auf 4 gesetzt
+        	y=new C{foo=5};                     // Speicheradresse wird neu initialisiert mit foo = 5
+        }
+        */
+		Console.WriteLine("y.foo: " + y.foo);   // Ausgabe: y.foo: 5
+        Console.WriteLine("z.foo: " + z.foo);   // Ausgabe: z.foo: 3
     }
 }
